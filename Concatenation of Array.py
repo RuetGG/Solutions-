daf48c16f80a35199bbc6eac01054d231907e572
RuetGG <152011601+RuetGG@users.noreply.github.com>
2024-07-07 14:56:55 +0300

    Create Concatenation of Array.py

---
+++ Concatenation of Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = nums
+        for i in range(len(nums)):
+            ans.append(nums[i])
+        return ans

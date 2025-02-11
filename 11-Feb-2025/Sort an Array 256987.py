c7f37a9f4c432afa12a07549b6370cbb7d277251
Ruth G Gebreyes <152011601+RuetGG@users.noreply.github.com>
2025-02-11 15:51:08 +0300

    Created 'Sort an Array' by A2SV Hub

---
+++ 11-Feb-2025/Sort an Array 256987.py
@@ -0,0 +1,16 @@
+# Problem: Sort an Array - https://leetcode.com/problems/sort-an-array/description/
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        maxi = max(nums)
+        mini = min(nums)
+        offset = -mini
+        count = [0] * (maxi + 1 - mini)
+        for i in nums:
+            count[i + offset] += 1
+
+        ans = []
+        for index in range(len(count)):
+            for _ in range(count[index]):
+                ans.append(index - offset)
+        return ans

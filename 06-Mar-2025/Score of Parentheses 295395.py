c410a6dcbf3467c855ebdee39d66449c8517daeb
Ruth G Gebreyes <152011601+RuetGG@users.noreply.github.com>
2025-03-06 23:42:14 +0300

    Created 'Score of Parentheses' by A2SV Hub

---
+++ 06-Mar-2025/Score of Parentheses 295395.py
@@ -0,0 +1,14 @@
+# Problem: Score of Parentheses - https://leetcode.com/problems/score-of-parentheses/
+
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        class Solution(object):
+        stack = []
+        score = 0
+        for char in s:
+            if char  == "(":
+                stack.append(score)
+                score = 0
+            else:
+                score = stack.pop() + max(2*score,1)
+        return score

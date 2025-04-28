246f536e5c05eb24df9f89ada8a6e7ea146f2687
Ruth G Gebreyes <152011601+RuetGG@users.noreply.github.com>
2025-04-28 22:52:55 +0300

    Created 'Count the Number of Complete Components' by A2SV Hub

---
+++ 28-Apr-2025/Count the Number of Complete Components 344339.py
@@ -0,0 +1,36 @@
+# Problem: Count the Number of Complete Components - https://leetcode.com/problems/count-the-number-of-complete-components/
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        count = 0
+        res = 0
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = set()
+        queue = deque()
+
+        def counter(queue, count):
+            sub = set()
+            nodes = set()
+            while queue:
+                node = queue.popleft()
+                nodes.add(node)
+                for nei in graph[node]:
+                    sub.add((min(node, nei), max(node, nei)))
+                    if nei not in visited:
+                        queue.append(nei)
+                        visited.add(nei)
+            size = len(nodes)
+            exp = size * (size - 1) // 2    
+            return 1 if len(sub) == exp else 0
+            
+        for i in range(n):
+            if i not in visited:
+                print(i)
+                queue.append(i)
+                res += counter(queue, count)
+        
+        return res 
